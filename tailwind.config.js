d6eeb53332bec6c437328b9256cd0d7ba6a53cf4
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-09-14 20:52:00 +0530

    ...

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      sans: ["Plus Jakarta Sans", "sans-serif"],
+    },
+    colors: {
+      black: "#030303",
+      cyan: "#61C4B7",
+      pink: "#F39E9E",
+      "medium-brown": "#7A746E",
+      "light-cream": "#FFF7F0",
+      "galactic-blue": "#755CDE",
+      "summer-yellow": "#F6A560",
+      "light-red": "#EB7565",
+      "dark-purple": "#552049",
+    },
+  },
+  plugins: [],
+};
